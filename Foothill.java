f4532ef3054cce67d5de8edd6731abc40b829ef4
derrick williams <dawilli4@gmail.com>
2013-12-06 23:03:48 -0800

    The original commit for Assignment#1

---
+++ Foothill.java
@@ -0,0 +1,29 @@
+/*
+ * Source program for Assignment #1
+ * Written by Derrick Williams
+ */
+
+public class Foothill
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Hello World!");
+      System.out.println("My family name is Williams.");
+      System.out.println("My Foothill Student ID# is 20175382.");
+      System.out.println("I noticed in the syllabus that " +
+         "besides the Public Forum (preferred), we also " +
+         "have the PSME Tutorial Center for assistance with " +
+         "any questions we have about the class.");
+   }
+}
+
+/* ----------------paste of run from console window---------------------------
+
+Hello World!
+My family name is Williams.
+My Foothill Student ID# is 20175382.
+I noticed in the syllabus that besides the Public Forum (preferred), we also hav
+e the PSME Tutorial Center for assistance with any questions we have about the c
+lass.
+
+----------------------------------------------------------------------------*/
