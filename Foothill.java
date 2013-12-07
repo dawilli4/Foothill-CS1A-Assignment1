389d41cf8e96cb5b629c34874531d754d64b82e1
dawilli4 <dawilli4@gmail.com>
2013-12-06 23:10:41 -0800

    Update Foothill.java

--- Foothill.java
+++ Foothill.java
@@ -1,6 +1,6 @@
 /*
  * Source program for Assignment #1
- * Written by Derrick Williams
+ * Written by 
  */
 
 public class Foothill
@@ -8,8 +8,8 @@ public class Foothill
    public static void main(String[] args)
    {
       System.out.println("Hello World!");
-      System.out.println("My family name is Williams.");
-      System.out.println("My Foothill Student ID# is 20175382.");
+      System.out.println("My family name is .");
+      System.out.println("My Foothill Student ID# is.");
       System.out.println("I noticed in the syllabus that " +
          "besides the Public Forum (preferred), we also " +
          "have the PSME Tutorial Center for assistance with " +
@@ -20,8 +20,8 @@ public class Foothill
 /* ----------------paste of run from console window---------------------------
 
 Hello World!
-My family name is Williams.
-My Foothill Student ID# is 20175382.
+My family name is .
+My Foothill Student ID# is.
 I noticed in the syllabus that besides the Public Forum (preferred), we also hav
 e the PSME Tutorial Center for assistance with any questions we have about the c
 lass.
